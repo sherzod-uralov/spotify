73bff3c2375d7997f8c7b9d2d7a78576964e1677
sherzod-uralov <sherzoduralov432@gmail.com>
2023-07-29 15:56:16 -0400

    a

--- src/components/Axios.jsx
+++ src/components/Axios.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Axios = () => {
   return (
     <div>
-        <h1>hello Qodirali</h1>
+        <h1>hello world</h1>
         <h2>sherzod</h2>
         <img src="" alt="" />1 
-        <h1>Qodir Ali</h1>   //Qodirali
+        <h1>Qodir aaaaa Ali</h1>
     </div>
   )
 }
