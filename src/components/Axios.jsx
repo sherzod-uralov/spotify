2d2d7a310c11a04f097ba17c7be6316181d7be9d
sherzod-uralov <sherzoduralov432@gmail.com>
2023-07-29 16:29:23 -0400

    freacr

--- src/components/Axios.jsx
+++ src/components/Axios.jsx
@@ -6,7 +6,7 @@ const Axios = () => {
         <h1>hello world</h1>
         <h2>sherzod</h2>
         <img src="" alt="" />1 
-        <h1>Qodir aaaaa Ali aaaaaa</h1>
+        <h1>Qodir aaaaa Ali aaaaaafafasf</h1>
     </div>
   )
 }
