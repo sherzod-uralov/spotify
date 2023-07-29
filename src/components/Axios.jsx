32c2f264dfac87fcd22e2eccc3e96d867abf8b35
Abdulaziz2003333 <108785546+Abdulaziz2003333@users.noreply.github.com>
2023-07-30 00:53:58 +0500

    Hello Sherzod
    Bla-bla

--- src/components/Axios.jsx
+++ src/components/Axios.jsx
@@ -4,7 +4,7 @@ const Axios = () => {
   return (
     <div>
         <h1>hello Qodirali</h1>
-        <h2>sherzod</h2>
+        <h2> hello sherzod</h2>
         <img src="" alt="" />1 
         <h1>Qodir Ali</h1>   //Qodirali
     </div>
