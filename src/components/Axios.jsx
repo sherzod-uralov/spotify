4c2360622b6582b5446b5c938f3b50637c656d18
Abdulaziz2003333 <108785546+Abdulaziz2003333@users.noreply.github.com>
2023-07-30 00:52:38 +0500

    Update Axios.jsx
    Qodirali boldi

--- src/components/Axios.jsx
+++ src/components/Axios.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Axios = () => {
   return (
     <div>
-        <h1>hello world</h1>
+        <h1>hello Qodirali</h1>
         <h2>sherzod</h2>
         <img src="" alt="" />1 
         <h1>Qodir Ali</h1>   //Qodirali
