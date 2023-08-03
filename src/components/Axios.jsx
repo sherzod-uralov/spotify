79419d49c38ff1a6f837dc8ca97f4352863b176d
sherzod-uralov <sherzoduralov432@gmail.com>
2023-08-03 12:15:58 -0400

    react

--- src/components/Axios.jsx
+++ src/components/Axios.jsx
@@ -6,22 +6,22 @@ const Axios = () => {
   const [state,setState] = useState() 
   function fetchData() {
     axios.get('https://restcountries.com/v3.1/all').then(e => {
-      setState(e)
+      setState(e.data)
     }).catch(err => console.log(err))
   }
   useEffect(() => {
     fetchData()
 },[])
-state.map(e => {
-  console.log(e);
-})
+console.log(state);
   return (
     <div>
-      {/* {state?.data?.map(e => {
-        <div className='cards'>
-          <img src={e.flaags.png} alt="" />
-        </div>
-      })}       */}
+      {
+        state.map(e => (
+          <div key={e.common.name}>
+            <img src={e.flags.png} alt="" />
+          </div>
+        ))
+      }
     </div>
   )
 }
