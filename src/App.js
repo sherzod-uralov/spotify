5720aa6373519868f9dc91c2c6bab45ecee16d9c
Abdulaziz2003333 <108785546+Abdulaziz2003333@users.noreply.github.com>
2023-08-03 20:33:37 +0500

    Create axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import './App.css';
-
+import "./App.css";
+import React, { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+  const [user, setUser] = useState(null);
+
+  const baseURL = "https://jsonplaceholder.typicode.com/posts/1";
+  useEffect(() => {
+    axios.get(baseURL).then((res) => setUser(res.data));
+  },[]);
+
+  return <div className="App">
+    <h1>salom</h1>
+    <h1>{user.title}</h1>
+  </div>;
 }
 
 export default App;
